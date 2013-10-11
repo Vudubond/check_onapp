bd6f8a164b5a122cf63e98ad18600e078f17a1cf
dangul <daniel@gullin.com>
2013-10-11 08:59:47 +0200

    1st

---
+++ auth.php
@@ -0,0 +1,11 @@
+<?php
+
+//$username = 'dangul';
+//$password = 'd8htda57';
+//$key 	  = '8fee34bb631dbf81620b632bc04dd97edc65365d';
+
+$username = 'dgullin';
+$password = 'XcG7tu57';
+$key      = '5dd58fb289e3035ae4fbe2a7241c1f2a149ffd4c';
+
+?>
